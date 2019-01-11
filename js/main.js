06cefc0e32029c2cff06e6e83f0fb62856486e0c
ericyin <ericyin322@gmail.com>
2019-01-11 13:27:54 +0800

    basic

--- js/main.js
+++ js/main.js
@@ -1,30 +1,14 @@
 $(document).ready(function() {
-	console.log($("#ct"));
 
-	$(".btn1").click(function(event) {
-		$("#ct").html("Hello jQuery")
-	});
-	$(".btn2").click(function(event) {
-		$("#myImage").attr('src', 'https://www.w3schools.com/js/pic_bulbon.gif');
-	});
-	$(".btn3").click(function(event) {
-		$("#myImage").attr('src', 'https://www.w3schools.com/js/pic_bulboff.gif');
-	});
-	$(".btn4").click(function(event) {
-		$("#ct2").css('font-size','32px')
-	});
-	$(".btn5").click(function(event) {
-		$("#ct2").css('font-size','16px')
-	});
-	$(".btn6").click(function(event) {
-		$("#ct3").hide()
-	});
-	$(".btn7").click(function(event) {
-		$("#ct3").show()
-	});
+	$(".story").on("click", function(){
+		$("#msg-box").text($(this).find(".story-more").text())
+		$("#popup-box").fadeIn(300);
 
-	$("body").on("click", "#ohh", function (event) {
-		console.log("!!")
 	});
-	$("<button id='ohh'>New Node</button>").appendTo("body")
-});
+	$("#popup-box").on("click", function(){
+		$("#popup-box").fadeOut(300, function(){
+			$("#msg-box").text("")
+		});
+
+	})
+})
