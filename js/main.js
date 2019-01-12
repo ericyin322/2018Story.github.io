1dd99e1cb927b6379de36c2e83e8862204f54b32
ericyin <ericyin322@gmail.com>
2019-01-12 23:05:57 +0800

    html clone

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
 	$(".story").on("click", function(){
-		$("#msg-box").text($(this).find(".story-more").text())
+		$("#msg-box").html($(this).find(".story-more").html())
 		$("#popup-box").fadeIn(300);
 		$("body").addClass("body-fix");
 	});
 	$("#popup-box").on("click", function(){
 		$("#popup-box").fadeOut(300, function(){
 			$("body").removeClass("body-fix");
-			$("#msg-box").text("")
+			$("#msg-box").html("")
 		});
 
 	})
