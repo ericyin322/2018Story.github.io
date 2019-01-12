526482ec4e5d66b07d17ff430f4f0e7360f1244f
ericyin <ericyin322@gmail.com>
2019-01-12 23:02:37 +0800

    popup fix body

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
 	$(".story").on("click", function(){
 		$("#msg-box").text($(this).find(".story-more").text())
 		$("#popup-box").fadeIn(300);
-		console.log("fadeIn")
+		$("body").addClass("body-fix");
 	});
 	$("#popup-box").on("click", function(){
 		$("#popup-box").fadeOut(300, function(){
+			$("body").removeClass("body-fix");
 			$("#msg-box").text("")
 		});
-		console.log("fadeOut")
+
 	})
 })
