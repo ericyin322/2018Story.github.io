cba9b14ddf8294fad594add9595cbbbbd3470697
ericyin <ericyin322@gmail.com>
2019-01-12 22:57:36 +0800

    初版

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 	$(".story").on("click", function(){
 		$("#msg-box").text($(this).find(".story-more").text())
 		$("#popup-box").fadeIn(300);
-
+		console.log("fadeIn")
 	});
 	$("#popup-box").on("click", function(){
 		$("#popup-box").fadeOut(300, function(){
 			$("#msg-box").text("")
 		});
-
+		console.log("fadeOut")
 	})
 })
