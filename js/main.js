541ed9cffc7e5c9c20e3604fa9313cf42587e5e5
ericyin <ericyin322@gmail.com>
2019-01-14 11:25:43 +0800

    change color, background, font

--- js/main.js
+++ js/main.js
@@ -5,11 +5,19 @@ $(document).ready(function() {
 		$("#popup-box").fadeIn(300);
 		$("body").addClass("body-fix");
 	});
+	
 	$("#popup-box").on("click", function(){
 		$("#popup-box").fadeOut(300, function(){
 			$("body").removeClass("body-fix");
 			$("#msg-box").html("")
 		});
+
+	});
+
+	$(document).on("scroll", function(){
+		let maskHeight = $("#content").offset().top-$(window).scrollTop();
+		let cssStr = `polygon(0% ${maskHeight}px, 0% 100%,100% 100%,100% ${maskHeight}px)`
+		$("#content-bg").css('clip-path',cssStr)
 
 	})
 })
